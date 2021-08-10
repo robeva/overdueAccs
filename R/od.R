65e8d7c50bfe93d2db6df368c437e7c80746b307
robeva <robeva99@googlemail.com>
2021-08-10 12:35:24 +0100

    remove files

--- R/od.R
+++ R/od.R
@@ -1,10 +1,2 @@
-install.packages("testthat")
-library(testthat)
-
-# Load functions file
-setwd("C:/Users/robev/Documents/Companies_House/overdueAccs")
-getwd()
-# source("R/functions.R")
-
 # Call function
 my_function(3,4)
