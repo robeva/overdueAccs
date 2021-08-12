afeedfb4d7366ba3d018dd8de0f422b7041dea51
robeva <83430736+robeva@users.noreply.github.com>
2021-08-12 13:52:44 +0100

    Delete od.R

--- R/od.R
+++
@@ -1,9 +0,0 @@
-# Read CSV file
-# ch_data <- read.csv("BasicCompanyData-2021-07-01-part1_6.csv", header = T)
-
-# pod <- overdue(ch_data)
-# pod
-
-# double % in the following expression prints the % symbol
-# .1 is 1 decimal place
-# sprintf("The percentage of overdue accounts is %.1f%%", pod)
